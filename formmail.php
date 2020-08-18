df8d2f8ea26254fe03a4a0ffbd7d166bbfdadc68
Shinesa Chowdhury <shinesac@yahoo.com>
2020-08-18 13:09:14 -0400

    latest update

--- formmail.php
+++ formmail.php
@@ -5,18 +5,27 @@
         $email=$_POST['email'];
         $phone=$_POST['phone'];
         $preferredcontact=$_POST['preferredcontact'];
-        $program=$_POST['[program'];
+        $program=$_POST['program'];
         $comments=$_POST['comments'];
 
         $to='kieowebsolutions@gmail.com';
         $subject='Form Submission';
-        $message="FirstName: ".$firstname."\n"."LastName: ".$lastName."\n"."Phone: ".$phone."\n"."PrefferedContact: ".$preferredcontact."\n"."Program: ".$program."\n". "Submitted the following: "."\n\n".$comments;
+        $message="FirstName: ".$firstname."\n"."LastName: ".$lastName."\n"."Phone: ".$phone."\n"."Email: ".$email."\n"."PrefferedContact: ".$preferredcontact."\n"."Program: ".$program."\n". "Submitted the following: "."\n\n".$comments;
         $headers="From: ".$email;
 
         if(mail($to, $subject, $message, $headers)){
             echo"<h1> Sent Successfully!</h1>";
+            echo $_POST["firstname"];
+            echo $_POST["lastName"];
+            echo $_POST["email"];
+            echo $_POST["phone"];
+            echo $_POST["preferredcontact"];
+            echo $_POST["comments"];
         } else {
             echo "Something went wrong! Please try again.";
         }
     }
-    ?>
+    ?> 
+
+
+
